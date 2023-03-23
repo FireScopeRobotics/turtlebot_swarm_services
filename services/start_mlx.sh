3116cfcca41ff694575972d5cc7b912285bf353b
Ayush Ghosh <ayushghosh21@gmail.com>
2023-03-23 16:07:37 -0400

    change ip addr

--- services/start_mlx.sh
+++ services/start_mlx.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bash -c "./home/ubuntu/turtlebot_swarm_services/mlx90640/examples/rawrgb | gst-launch-1.0 fdsrc blocksize=2304 ! multiudpsink clients=192.168.23.44:5000,192.168.23.44:5001"
+bash -c "./home/ubuntu/turtlebot_swarm_services/mlx90640/examples/rawrgb | gst-launch-1.0 fdsrc blocksize=2304 ! multiudpsink clients=192.168.23.7:5000,192.168.23.7:5001"
